4261ff333cde78c5eb8e408c09c0c8eecfe10e78
Keywhee <adorablefluffykitties@gmail.com>
2020-05-06 21:17:38 -0400

    trying ou another comment system D:

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 /********************************************************
 Our main js file.. if we ever use it :')
 ********************************************************/
+import commentBox from 'commentbox.io';
+
+commentBox('5763880655519744-proj');
