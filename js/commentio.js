da15cc3329778b0a22cd16b523c38dd438e47b3e
Keywhee <adorablefluffykitties@gmail.com>
2020-05-06 21:25:00 -0400

    js

---
+++ js/commentio.js
@@ -0,0 +1,3 @@
+import commentBox from 'commentbox.io';
+
+commentBox('5763880655519744-proj');
--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
 /********************************************************
 Our main js file.. if we ever use it :')
 ********************************************************/
-import commentBox from 'commentbox.io';
-
-commentBox('5763880655519744-proj');
